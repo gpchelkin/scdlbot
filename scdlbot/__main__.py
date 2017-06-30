0e9a257dda271fd39aeaee4b048b985ed4f19d37
gpchelkin <george@pchelk.in>
2017-06-30 14:39:07 +0300

    Small fixes and refactoring

--- scdlbot/__main__.py
+++ scdlbot/__main__.py
@@ -14,8 +14,8 @@ def main():
     no_clutter_chat_ids = list(map(int, os.getenv('NO_CLUTTER_CHAT_IDS', '').split(',')))
     dl_dir = os.path.expanduser(os.getenv('DL_DIR', '~'))
     use_webhook = bool(int(os.getenv('USE_WEBHOOK', '0')))
-    app_port = int(os.getenv('PORT', '5000'))
     app_url = os.getenv('APP_URL', '')
+    app_port = int(os.getenv('PORT', '5000'))
     bin_path = os.getenv('BIN_PATH', '')
     scdlbot = SCDLBot(tg_bot_token, botan_token, bin_path,
                       sc_auth_token, store_chat_id, no_clutter_chat_ids, dl_dir)
