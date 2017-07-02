9bfe4439b5e4c30164f686ccda8e6eb7ccbee766
gpchelkin <george@pchelk.in>
2017-07-02 20:46:23 +0300

    link parse mode

--- scdlbot/scdlbot.py
+++ scdlbot/scdlbot.py
@@ -178,7 +178,7 @@ class SCDLBot:
             logger.debug(event_name)
             self.botan.track(update.message, event_name) if self.botan else None
             link_message = bot.send_message(chat_id=chat_id, reply_to_message_id=update.message.message_id,
-                                            parse_mode='Markdown', text="\n".join(urls.values()))
+                                            text=" \n ".join(urls.values()))
 
     def dl_command_callback(self, bot, update, args=None):
         chat_id = update.message.chat_id
