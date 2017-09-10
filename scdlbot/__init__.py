368b83f83d2d0253f44569346c9b2244f39834ff
gpchelkin <george@pchelk.in>
2017-09-10 18:07:07 +0300

    v0.7.5.1

--- scdlbot/__init__.py
+++ scdlbot/__init__.py
@@ -3,7 +3,7 @@
 """Top-level package for Music Downloader Telegram Bot."""
 
 # version as tuple for simple comparisons
-VERSION = (0, 7, 5)
+VERSION = (0, 7, 5, 1)
 
 __author__ = """George Pchelkin"""
 __email__ = 'george@pchelk.in'
