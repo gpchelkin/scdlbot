95df47cfa6130e8e688dfc6602e8b53880d61897
George Pchelkin <gpchelkin@users.noreply.github.com>
2019-11-02 12:51:41 +0300

    add __version__ again for docs building

--- scdlbot/__init__.py
+++ scdlbot/__init__.py
@@ -3,4 +3,5 @@
 """Top-level package for Music Downloader Telegram Bot."""
 
 __author__ = """George Pchelkin"""
-__email__ = 'george@pchelk.in'
+__email__ = "george@pchelk.in"
+__version__ = "0.9.24"
