af2731371cc29018220e344f8e6effb7fcae6032
George Pchelkin <george@pchelk.in>
2023-06-12 19:55:14 +0100

    fix captions with right-to-left hashtags, part 3

--- scdlbot/__main__.py
+++ scdlbot/__main__.py
@@ -1010,7 +1010,7 @@ def download_url_and_send(
                 info_dict = ydl.YoutubeDL(ydl_opts).extract_info(url, download=False)
                 if "description" in info_dict:
                     # TODO better handle right-to-left hashtags https://www.instagram.com/reel/CtZbNhtrJv3/
-                    add_description = info_dict["description"][:800].replace("#", "")
+                    add_description = info_dict["description"][:800].replace("#", "").replace(".", "")
         except Exception as exc:
             print(exc)
             logger.debug("%s failed: %s", cmd_name, url)
